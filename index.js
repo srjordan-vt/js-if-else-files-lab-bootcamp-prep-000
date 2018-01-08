d26eae70402a7a421c10d7f6cf7bbab92572b362
Sara Jordan <sara.jordan@gmail.com>
2018-01-08 01:59:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,3 +11,8 @@ if (name === "Susan") {
 } else {
   console.log("Expected the name to be Susan")
 }
+if (height === "74") {
+    console.log("The height is correct")
+} else {
+  console.log("Expected the height to be 70")
+}
